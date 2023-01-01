9c6b58f2db10bade65993d5fd187748e22efcea6
entbhone <entbhonemyintmo@gmail.com>
2023-01-02 00:15:44 +0630

    remember undone Task feate DONE without del Fn

--- app.js
+++ app.js
@@ -3,16 +3,60 @@ let taskId = 0,
   containerArray = [],
   taskObj,
   jsonConvertedArray,
-  jsonConvertTaskObj
+  dataFromLocalStorage,
+  arrayConvertedJson
 
 
 
 window.addEventListener('load', () => {
+  createTaskFn()
+  const finalTaskID = arrayConvertedJson[arrayConvertedJson.length-1].taskId
+  taskId = finalTaskID + 1
+} )
+
+
+userInputTag.addEventListener('change', (event) => {
+
+  containerArray = []
+
+  const inputValue = event.target.value
+
+  // Object Constructer
+  function MyObject(taskName, taskId) {
+    this.taskName = taskName
+    this.taskId = taskId
+  }
+
+  taskObj = new MyObject(inputValue, taskId);
+  
+  containerArray.push(taskObj)
+
+  
+  if (localStorage.hasOwnProperty('previousData')) {
+    dataFromLocalStorage = localStorage.getItem('previousData')
+    arrayConvertedJson = JSON.parse(dataFromLocalStorage)
+    const concatArray = [...arrayConvertedJson, ...containerArray]
+    localStorage.setItem('previousData', JSON.stringify(concatArray))
+  } else {
+    localStorage.setItem('previousData', JSON.stringify(containerArray))
+  }
+
+  createTaskFn()
+
+  taskId++
+  event.target.value = ''
+})
+
+
+
+function createTaskFn () {
 
   if (localStorage.hasOwnProperty('previousData')) {
-    const dataFromLocalStorage = localStorage.getItem('previousData')
-    const arrayConvertedJson = JSON.parse(dataFromLocalStorage)
 
+    document.querySelector('.listContainer').innerHTML = ''
+
+    dataFromLocalStorage = localStorage.getItem('previousData')
+    arrayConvertedJson = JSON.parse(dataFromLocalStorage)
     arrayConvertedJson.forEach(task => {
       const taskTag = `
           <div
@@ -30,108 +74,13 @@ window.addEventListener('load', () => {
 
           document.querySelector('.listContainer').innerHTML += taskTag
 
-
-          
-          
-
     });
 
   }
 
-
-})
-
-
-
-
-
-
-userInputTag.addEventListener('change', (event) => {
-
-
-  const inputValue = event.target.value
-  applyTaskListFn(inputValue, taskId)
-  
-  localStorage.removeItem('previousData')
-
-  // Object Constructer
-  function MyObject(param1 = 'default1', param2 = 'default2') {
-    this.taskName = param1
-    this.taskId = param2
-  }
-
-  taskObj = new MyObject(inputValue, taskId);
-  
-  containerArray.push(taskObj)
-
-  jsonConvertedArray = JSON.stringify(containerArray)
-  localStorage.setItem('previousData', jsonConvertedArray)
-
-
-
-  taskId++
-  event.target.value = ''
-})
-
-
-
-
-
-
-
-
-
-
-/* -----------------------------for Local Storage----------------------------------------------------------------- */
-// console.log(containerArray)
-
-
-
-
-
-/*---------------------------------DOM Create process----------------------------------------------------------- */
-function applyTaskListFn(userInput, id) {
-  const listTag = document.createElement('div')
-  listTag.classList.add(
-    'listTag',
-    'list-group-item',
-    'd-flex',
-    'justify-content-between',
-    'align-items-center',
-    'hideTag',
-  )
-  listTag.id = id
-  setTimeout(() => {
-    listTag.style.opacity = 1
-  }, 100)
-
-  const innerTaskWarpper = document.createElement('div')
-  innerTaskWarpper.classList.add(
-    'd-flex',
-    'justify-content-between',
-    'align-items-center',
-  )
-
-  const radioInput = document.createElement('input')
-  radioInput.classList.add('form-check-input', 'mt-0', 'me-3')
-  radioInput.type = 'radio'
-
-  const taskLabel = document.createElement('label')
-  taskLabel.classList.add('fw-bold', 'form-check-label', 'textWarp')
-  taskLabel.textContent = userInput
-
-  innerTaskWarpper.append(radioInput, taskLabel)
-
-  const deadlinSpan = document.createElement('span')
-  deadlinSpan.classList.add('badge', 'bg-danger', 'rounded-pill')
-  deadlinSpan.textContent = 'tomorrow'
-
-  listTag.append(innerTaskWarpper, deadlinSpan)
-
-  document.querySelector('.listContainer').append(listTag)
-
-  radioInput.addEventListener('click', removeTaskFn)
 }
+
+
 
 function removeTaskFn (event) {
   const targetTag = event.target.parentNode.parentNode.id
@@ -142,6 +91,7 @@ function removeTaskFn (event) {
     
   }, 500)
 }
+
 
 
 function localStorageDataRemoveFn (IdPara) {
